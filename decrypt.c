bfbd8c3eef5819ce5cad0b096852dc8c6a42281c
LunarStone292 <93814414+LunarStone292@users.noreply.github.com>
2023-01-22 18:58:00 +0100

    Add files via upload

---
+++ decrypt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    char frase[100];
+    int key;
+
+    system("cls");
+
+    printf("enter the phrase to encrypt: ");
+    gets(frase);
+
+    printf("\nEnter the numeric key: ");
+    scanf("%d", &key);
+
+    printf("\n");
+    for(int i = 0; i < strlen(frase); i++){
+        if(i % 2 == 0){
+            frase[i] -= key;
+        }
+        else{
+            frase[i] += key;
+        }
+        printf("%c", frase[i]);
+    }
+    printf("\n");
+}
